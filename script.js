67209ad0d020142da9510fe225c1e26408439f3e
iamlistless <98135875+iamlistless@users.noreply.github.com>
2023-09-29 16:40:56 -0700

    Add permanent hover effect using JavaScript

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-const gridContainer = document.querySelector(".gridContainer")
+const gridContainer = document.querySelector(".gridContainer");
 
 for (i = 0; i < 256; i++) {
     const pixel = document.createElement("div")
     pixel.classList.add("pixel")
     gridContainer.appendChild(pixel)
-}
     
+    pixel.addEventListener("mousemove", () => {
+        pixel.style.backgroundColor = "gray";
+        }); 
+
+    // let isDrawing = false;
+
+    // pixel.addEventListener("mousedown", () => {
+    //     pixel.style.backgroundColor = "gray";
+    //     isDrawing = true;
+    // });
+    
+    // pixel.addEventListener("mousemove", () => {
+    //     if (isDrawing) {
+    //         pixel.style.backgroundColor = "gray";
+    //     } 
+    // });
+
+    // pixel.addEventListener("mouseup", () => {
+    //     if (isDrawing) {
+    //         isDrawing = false;
+    //     }
+    // })
+}
 
