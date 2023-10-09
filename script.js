efc6d7497777aae77e10a5850884764dc0d97c94
iamlistless <98135875+iamlistless@users.noreply.github.com>
2023-10-09 14:29:10 -0700

    Add shading effect for defaultMode

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const defaultGridSize = 32; 
-const defaultPixelColor = 'white';
+// const defaultPixelColor = 'white';
 const defaultMode = 'colorMode';
 
 let currentGridSize = defaultGridSize;
-let currentPixelColor = defaultPixelColor;
+// let currentPixelColor = defaultPixelColor;
 let currentMode = defaultMode;
 
 function setCurrentMode(newMode) {
@@ -14,6 +14,8 @@ const grid = document.getElementById('grid');
 const colorButtons = document.querySelectorAll('.colorButton')
 const colorButton = [...colorButtons]
 const clearButton = document.querySelector('.clearButton')
+
+let rgbColor = `rgb(229, 229, 229)`
 
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
@@ -29,9 +31,6 @@ colorButtons.forEach (colorButton => {
 clearButton.addEventListener('click', () => {
     clearGrid()
 })
-
-// let gridStyles = getComputedStyle(grid);
-// let gridSize = gridStyles.getPropertyValue('--size');
 
 function createGrid(currentGridSize) {
     grid.style.gridTemplateColumns = `repeat(${currentGridSize}, 1fr)`;
@@ -51,7 +50,12 @@ function changeColor(e) {
         return
     }
     else if (currentMode === 'colorMode') {
-        e.target.style.backgroundColor = '#e5e5e5';
+        if (e.target.style.backgroundColor === '') {
+            e.target.style.backgroundColor = rgbColor;
+        }
+        else if (e.target.style.backgroundColor === e.target.style.backgroundColor) {
+            e.target.style.backgroundColor = shadeColor(e.target.style.backgroundColor);
+        }
     }
     else if (currentMode === 'rainbowMode') {
         const randomR = Math.floor(Math.random() * 256)
@@ -59,7 +63,6 @@ function changeColor(e) {
         const randomB = Math.floor(Math.random() * 256)
         e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
     }
-    // console.log(e.target.style.backgroundColor)
 }
 
 function makeGridSize() {
@@ -83,11 +86,64 @@ window.onload = () => {
     setCurrentMode(defaultMode)
 }
 
+function shadeColor(rgbColor) {
+    let firstRgbIndex = rgbColor.indexOf(',');
+    let R = parseInt(rgbColor.substring(4, firstRgbIndex));
+    let middleRgbIndex = rgbColor.lastIndexOf(',');
+    let G = parseInt(rgbColor.substring([firstRgbIndex + 2], middleRgbIndex));
+    let lastRgbIndex = rgbColor.lastIndexOf(')');
+    let B = parseInt(rgbColor.substring([middleRgbIndex + 2], lastRgbIndex));
 
-// function shadeColor(e) {
-//     let R = parseInt(e.target.style.backgroundColor.substring(4,7));
-//     let G = parseInt(e.target.style.backgroundColor.substring(9,12));
-//     let B = parseInt(e.target.style.backgroundColor.substring(14,17));
+    R = parseInt(R * (100 + -10) / 100);
+    G = parseInt(G * (100 + -10) / 100);
+    B = parseInt(B * (100 + -10) / 100);
+
+    R = (R<255)?R:255;  
+    G = (G<255)?G:255;
+    B = (B<255)?B:255;  
+
+    R = Math.round(R)
+    G = Math.round(G)
+    B = Math.round(B)
+
+    return rgbColor = `rgb(${R}, ${G}, ${B})`;
+}
+
+// function hexToRGB(hexColor) {
+//     let R = parseInt(hexColor.substring(4,7));
+//     let G = parseInt(hexColor.substring(9,12));
+//     let B = parseInt(hexColor.substring(14,17));
+// }
+// function shadingTool(div) {
+//     if (!(div.getAttribute('data-color') == hexColor)) {
+//         console.log(div.getAttribute('data-color'));
+//         console.log(hexColor);
+//         div.setAttribute('data-shade', '0');
+    
+//         let shadeValue = parseFloat(div.getAttribute('data-shade')) + 0.1;
+//         console.log(shadeValue);
+//         shadeValue = parseFloat(shadeValue.toFixed(2));
+//         shadeValue = Math.min(shadeValue, 1);
+    
+//         div.setAttribute('data-shade', shadeValue);
+
+//         const indexOfLast = hexToRGB(hexColor).lastIndexOf(')');
+//         console.log(hexToRGB(hexColor));
+//         console.log(indexOfLast);
+//         rgbColor = `${hexToRGB(hexColor).slice(0, indexOfLast)}, ${shadeValue})`;
+//         console.log(rgbColor);
+//         div.style.backgroundColor = rgbColor;
+//         console.log(div.style.backgroundColor);
+//         div.setAttribute('data-color', hexColor);
+//         console.log(div);
+//     }
+// }
+
+// function hexToRGB(hexColor) {
+//     let R = parseInt(hexColor.substring(1,3), 16);
+//     let G = parseInt(hexColor.substring(3,5), 16);
+//     let B = parseInt(hexColor.substring(5,7), 16);
+
 
 //     R = parseInt(R * (100 + -10) / 100);
 //     G = parseInt(G * (100 + -10) / 100);
@@ -101,8 +157,10 @@ window.onload = () => {
 //     G = Math.round(G)
 //     B = Math.round(B)
 
-//     e.target.style.backgroundColor = `rgb(${R}+${G}+${B})`;
+//     return `rgb(${R}, ${G}, ${B})`;
 // }
+
+
 
 
 // function clearGrid() {
